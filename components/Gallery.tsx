2c9dc88c4c6218950354f517041313f8b78ea283
jodrack <drack.sir01@gmail.com>
2022-11-19 16:04:02 +0200

    [feature]Add gallery compoent

---
+++ components/Gallery.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { sliders } from './data/sliderData';
+import {FaArrowAltCircleLeft} from 'react-icons/fa'
+import {FaArrowAltCircleRight} from 'react-icons/fa'
+
+export enum Direction {
+  Left = 'LEFT',
+  Right = 'RIGHT',
+}
+
+export default function Gallery() {
+  const [current, setCurrent] = useState(0);
+  const length = sliders.length - 1;
+
+  const handleSlide = (direction: Direction) => {
+    if (direction === 'LEFT') {
+      setCurrent(current !== 0 ? current - 1 : length);
+      return;
+    }
+    setCurrent(current !== length ? current + 1 : 0);
+  };
+
+  return (
+    <div className='max-w-[1240px] mx-auto mb-[8rem]' id='gallery'>
+      <h1 className='text-center text-2xl text-bold p-8'>Gallery</h1>
+
+      <div className='relative flex justify-center w-[1240px] h-[500px] p-4'>
+      {sliders.map((slider, index) => (
+            <div
+            style={{height: '100%', width: '100%'}}
+            key={index}
+            className={index === current ? 'opacity-[1] ease-out duration-1000' : 'opacity-[0]'}
+            >
+            <FaArrowAltCircleLeft onClick={() => handleSlide(Direction.Left)} size={40} className='absolute top-[50%] left-[30px] z-[2] cursor-pointer text-white/70'/>
+            {index === current && (
+                <Image
+                src={slider.image}
+                alt='/'
+                fill={true}
+                style={{objectFit: 'cover'}}
+              />
+            )}
+            <FaArrowAltCircleRight onClick={() => handleSlide(Direction.Right)} size={40} className='absolute top-[50%] right-[30px] z-[2] text-white/70'/>
+            </div>
+            ))}
+            </div>
+            </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Hero from '../components/Hero';
+import Gallery from '../components/Gallery';
 
 export default function Home() {
   return (
     <>
-      <Hero heading='Capure Photography' message='Capure moments in nature and keep them alive' />
+      <Hero
+        heading='Capure Photography'
+        message='Capure moments in nature and keep them alive'
+      />
+      <Gallery />
     </>
   );
 }
